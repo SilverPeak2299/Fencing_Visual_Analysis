86a1e80473a45084cd1a1d5fd5d10f5a0ac7cc6d
SilverPeak2299 <dannydickerson03@gmail.com>
2025-05-30 12:16:17 +1000

    perf: Use YOLOv11s pose model

--- src/video_analysys.py
+++ src/video_analysys.py
@@ -6,7 +6,7 @@ class VideoAnalysys:
     model: YOLO
     
     def __init__(self) :
-        self.model = YOLO("./models/yolo11l-pose.pt")
+        self.model = YOLO("./models/yolo11s-pose.pt")
         
     def analyze_frame(self, frame):
         results = self.model(frame)
